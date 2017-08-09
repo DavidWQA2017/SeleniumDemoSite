eb80f24f13959b80c956082e4fdd2bf779dfde43
David <David.Walters@qa.com>
2017-08-09 14:41:59 +0000

    added basic spreadsheet reader

--- SeleniumLogin/src/test/java/JavaTest.java
+++ SeleniumLogin/src/test/java/JavaTest.java
@@ -17,6 +17,7 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 //import java.util.function.Function;
 
 import java.io.IOException;
+import java.util.List;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -33,45 +34,140 @@ public class JavaTest
 
     private static ExtentReports report;
     private static ExtentTest test;
+    private static ExtentTest test2;
     private static String reportFilePath = "C:\\Users\\Administrator\\IdeaProjects\\SeleniumLogin\\report.html";
 
     @Test
     public void test()
     {
-        String filename = "IAMAFILE";
+        String filename = "ScreenShotTest1";
+        String[] spreadsheetInfo = new String[4];
+        int i = 0;
+
+        SpreadSheet sheetReader = new SpreadSheet(System.getProperty("user.dir") + "/src/main/resources/TestInfo.xlsx");
+        List<String> row = sheetReader.readRow(1, "TestSheet");
+
+        for(String cell : row)
+        {
+            spreadsheetInfo[i] = cell;
+            i++;
+        }
+
+        String testID = spreadsheetInfo[0];
+        String testDescrip = spreadsheetInfo[1];
+        String testUsername = spreadsheetInfo[2];
+        String testPassword = spreadsheetInfo[3];
+
+
+
+
+
         //create user for test
         webDriver.navigate().to("http://www.thedemosite.co.uk/addauser.php");
-        createUser.enterUsernName("usertest");
-        createUser.enterPassword("usertest");
+        createUser.enterUsernName(testUsername);
+        test.log(Status.INFO, "username has been created");
+        createUser.enterPassword(testPassword);
+        test.log(Status.INFO, "password has been created for the account");
         createUser.createUser();
+        test.log(Status.INFO, "user has been created");
         //webDriver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[1]/td[2]/p/input")).sendKeys("usertest");
         //webDriver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[2]/td[2]/p/input")).sendKeys("usertest");
         //webDriver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input")).click();
 
         //input login
         webDriver.navigate().to("http://www.thedemosite.co.uk/login.php");
-
         //webDriver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/p/input")).sendKeys("usertest");
         //webDriver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/p/input")).sendKeys("usertest");
         //webDriver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/p/input")).click();
-        logInPage.enterUsernName("usertest");
-        logInPage.enterPassword("usertest");
+        logInPage.enterUsernName(testUsername);
+        test.log(Status.INFO, "username has been entered");
+        logInPage.enterPassword(testPassword);
+        test.log(Status.INFO, "password has been entered");
         logInPage.login();
+        test.log(Status.INFO, "login has been attempted");
         try
         {
             Screenshot.take(webDriver, filename);
             test.addScreenCaptureFromPath("C:\\Users\\Administrator\\IdeaProjects\\SeleniumLogin\\IAMAFILE.jpg");
+            test.log(Status.INFO, "a screenshot of the login attempt has been created  ");
         }
         catch (IOException e)
         {
-            System.out.println("I broke");
+            test.log(Status.WARNING, "Screenshot could not be taken input/output exception" + e);
         }
 
 
 
         //logInPage.login();
         Assert.assertEquals("Login unsuccessful","**Successful Login**", wait.until(webDriver -> webDriver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b")).getText()));
-        test.log(Status.PASS, "it has passed" );
+        test.log(Status.PASS, "Log in is successful" );
+
+    }
+
+    @Test
+    public void LoginFail_Test()
+    {
+        String filename = "ScreenShotTest1";
+        String[] spreadsheetInfo = new String[4];
+        int i = 0;
+
+        SpreadSheet sheetReader = new SpreadSheet(System.getProperty("user.dir") + "/src/main/resources/TestInfo.xlsx");
+        List<String> row = sheetReader.readRow(2, "TestSheet");
+
+        for(String cell : row)
+        {
+            spreadsheetInfo[i] = cell;
+            i++;
+        }
+
+        String testID = spreadsheetInfo[0];
+        String testDescrip = spreadsheetInfo[1];
+        String testUsername = spreadsheetInfo[2];
+        String testPassword = spreadsheetInfo[3];
+
+
+
+        //create user for test
+        webDriver.navigate().to("http://www.thedemosite.co.uk/addauser.php");
+        createUser.enterUsernName("usertest");
+        test2.log(Status.INFO, "username has been created");
+        createUser.enterPassword(testPassword);
+        test2.log(Status.INFO, "password has been created for the account");
+        createUser.createUser();
+        test2.log(Status.INFO, "user has been created");
+
+
+        //input login
+        webDriver.navigate().to("http://www.thedemosite.co.uk/login.php");
+        logInPage.enterUsernName(testUsername);
+        test2.log(Status.INFO, "username has been entered");
+        logInPage.enterPassword(testPassword);
+        test2.log(Status.INFO, "password has been entered");
+        logInPage.login();
+        test2.log(Status.INFO, "login has been attempted");
+
+        try
+        {
+            Screenshot.take(webDriver, filename);
+            test2.addScreenCaptureFromPath("C:\\Users\\Administrator\\IdeaProjects\\SeleniumLogin\\IAMAFILE2.jpg");
+            test2.log(Status.INFO, "a screenshot of the login attempt has been created  ");
+        }
+        catch (IOException e)
+        {
+            test2.log(Status.WARNING, "Screenshot could not be taken input/output exception" + e);
+        }
+
+
+        //logInPage.login();
+        try
+        {
+            Assert.assertEquals("Login unsuccessful", "**Successful Login**", wait.until(webDriver -> webDriver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b")).getText()));
+            test2.log(Status.PASS, "Log in is successful" );
+        }
+        catch (AssertionError e)
+        {
+            test2.log(Status.FAIL, "could not login" );
+        }
 
     }
 
@@ -101,6 +197,7 @@ public class JavaTest
         extentHtmlReporter.config().setDocumentTitle("DocumentTitle");
         report.attachReporter(extentHtmlReporter);
         test = report.createTest("TestName");
+        test2 = report.createTest("TestName");
     }
 
 
